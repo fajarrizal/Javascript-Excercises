7af140c1c120f05287c32584c7371c0b37430f09
fajarrizal <fajarrizal123@gmail.com>
2020-11-05 14:54:59 +0700

    initialize commit

---
+++ template_literals_konsep/string_biasa.js
@@ -0,0 +1,8 @@
+/*
+ menulis string menggunakan kutip dua.
+ menggabungkan string menggunakan operator +
+*/
+const nama = "Rizal fajar";
+const menyapa = "Hallo" + nama + ", sehat sehat selalu ya.";
+
+console.log(menyapa);
---
+++ template_literals_konsep/string_templateliteral.js
@@ -0,0 +1,8 @@
+/*
+ menulis string menggunakan template literals
+ memasukkan variabel dengan sintaks ${}
+*/
+const nama = "Rizal fajar";
+const sapa = `Halo ${nama}, sehat sehat selalu`;
+
+console.log(sapa);
