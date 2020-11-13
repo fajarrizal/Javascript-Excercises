ed6d7d8ea6af5337fa935f3e8a5ba0ed67606ac9
fajarrizal <fajarrizal123@gmail.com>
2020-11-13 16:08:30 +0700

    initialize commit

--- array dan object/Object.js
+++ array dan object/Object.js
@@ -18,3 +18,32 @@ const NamaOrang = {
 };
 console.log(NamaOrang.fullName());
 console.log(NamaOrang.idealweight());
+
+// object part 2
+const negara = {
+    ID : {
+        provinsi : ['Aceh', 'Jawa Barat', 'Sumatera Selatan'],
+        city : ['Banda Aceh', 'Bandung', 'Padang', 'Semarang'],
+    },
+    USA : {
+        provinsi : ['Texas', 'Virginia', 'Utah'],
+        city : ['New York', 'Los Angeles', 'Washington', 'Chicago']
+    },
+};
+console.log(negara.ID);
+
+// akses object tapi hanya salah satu isi array tersebut.
+console.log(negara.USA.provinsi);
+
+// Object menggunakan *Optional Chaning dan *Nullish Coalescing Operator
+// mengakses suatu object tetapi variabel array tidak ada di deklarasikan
+// pada masalah ini variabel object negara RUS dan arraynya tidak di deklarasikan
+console.log(negara?.RUS?.provinsi);
+//  Ekspetasi console akan menjadi *Undefined
+
+// sekarang menggunakan keduanya Optional chaning dan Nullish Coalescing
+// hasil akan berbeda
+console.log(negara?.RUS?.provinsi ?? "Tidak Ditemukan");
+// penjelasannya : apapun value kiri undefined, null / false (tidak ketemu) maka 
+// value kanan "tidak ditemukan" akan jalan 
+// dan itu sebaliknya (misal kiri ditemukan valuenya) maka yang value kanan tidak diperlukan
